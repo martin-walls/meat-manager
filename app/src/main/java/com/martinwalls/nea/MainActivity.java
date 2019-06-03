8e9f665307d76eea59a90daabcc2a8b2fa9a8f2e
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-06-03 21:21:07 +0100

    new stock page

--- app/src/main/java/com/martinwalls/nea/MainActivity.java
+++ app/src/main/java/com/martinwalls/nea/MainActivity.java
@@ -1,5 +1,6 @@
 package com.martinwalls.nea;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.navigation.NavigationView;
 import androidx.core.view.GravityCompat;
@@ -78,8 +79,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
+        if (id == R.id.nav_stock) {
+            Intent intent = new Intent(this, NewStockActivity.class);
+            startActivity(intent);
+        }
+            // Handle the camera action
 //        } else if (id == R.id.nav_gallery) {
 //
 //        } else if (id == R.id.nav_slideshow) {
---
+++ app/src/main/java/com/martinwalls/nea/NewStockActivity.java
@@ -0,0 +1,14 @@
+package com.martinwalls.nea;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.martinwalls.nea.R;
+
+public class NewStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_stock);
+    }
+}
