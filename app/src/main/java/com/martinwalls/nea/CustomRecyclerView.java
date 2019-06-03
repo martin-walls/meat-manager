355c407e170e69813577f9fc3963618d660f7b34
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-06-03 20:47:35 +0100

    Added CustomRecyclerView

---
+++ app/src/main/java/com/martinwalls/nea/CustomRecyclerView.java
@@ -0,0 +1,62 @@
+package com.martinwalls.nea;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomRecyclerView extends RecyclerView {
+
+    private View emptyView;
+
+    private AdapterDataObserver dataObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            updateEmptyView();
+        }
+    };
+
+    public CustomRecyclerView(Context context) {
+        super(context);
+    }
+
+    public CustomRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+    }
+
+    @Override
+    public void setAdapter(RecyclerView.Adapter adapter) {
+        if (getAdapter() != null) {
+            getAdapter().unregisterAdapterDataObserver(dataObserver);
+        }
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(dataObserver);
+        }
+        super.setAdapter(adapter);
+        updateEmptyView();
+    }
+
+    private void updateEmptyView() {
+        if (emptyView != null && getAdapter() != null) {
+            boolean showEmptyView = getAdapter().getItemCount() == 0;
+            if (showEmptyView) {
+                emptyView.setVisibility(VISIBLE);
+                setVisibility(GONE);
+            } else {
+                emptyView.setVisibility(GONE);
+                setVisibility(VISIBLE);
+            }
+        }
+    }
+}
