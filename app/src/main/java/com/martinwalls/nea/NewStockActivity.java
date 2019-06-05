4b80e982c220033d7cf4ed9ee38a53b85986fb20
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-06-05 20:13:20 +0100

    search results shown for each input field

--- app/src/main/java/com/martinwalls/nea/NewStockActivity.java
+++ app/src/main/java/com/martinwalls/nea/NewStockActivity.java
@@ -15,6 +15,7 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class NewStockActivity extends AppCompatActivity {
 
@@ -22,17 +23,16 @@ public class NewStockActivity extends AppCompatActivity {
 
     private SearchItemAdapter itemAdapter;
     private ArrayList<String> searchResultList = new ArrayList<>();
-    private TextView emptyView;
-    private CustomRecyclerView recyclerView;
-    private RelativeLayout recyclerViewWrapper;
+    private RelativeLayout searchResultsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_stock);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        // store reference to each row to show/hide later
         inputViews.put("product", R.id.input_layout_product);
         inputViews.put("supplier", R.id.input_layout_supplier);
         inputViews.put("quantity", R.id.input_row_quantity);
@@ -42,56 +42,38 @@ public class NewStockActivity extends AppCompatActivity {
 
         // setup recycler view
         itemAdapter = new SearchItemAdapter(searchResultList);
-        emptyView = findViewById(R.id.no_results);
-        recyclerView = findViewById(R.id.recycler_view_results);
+        TextView emptyView = findViewById(R.id.no_results);
+        CustomRecyclerView recyclerView = findViewById(R.id.recycler_view_results);
         recyclerView.setAdapter(itemAdapter);
         recyclerView.setEmptyView(emptyView);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        recyclerViewWrapper = findViewById(R.id.recycler_view_wrapper);
+        searchResultsLayout = findViewById(R.id.search_results_layout);
 
 
-        final TextInputEditText inputProduct = findViewById(R.id.edit_text_product);
-        final TextInputLayout inputLayoutProduct = findViewById(R.id.input_layout_product);
-        inputLayoutProduct.setEndIconVisible(false);
+        // todo add new product option at top of list - show what the user is typing with add new option if not exists
 
 
-        inputProduct.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    inputLayoutProduct.setEndIconVisible(true);
-                    inputOnClick("product");
-                }
-            }
-        });
 
-        inputLayoutProduct.setEndIconOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputProduct.setText("");
-                inputProduct.clearFocus();
-                inputLayoutProduct.setEndIconVisible(false);
-                cancelInputProductOnClick();
-            }
-        });
+        setListeners("product",
+                (TextInputLayout) findViewById(R.id.input_layout_product),
+                (TextInputEditText) findViewById(R.id.edit_text_product));
 
-        inputProduct.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        setListeners("supplier",
+                (TextInputLayout) findViewById(R.id.input_layout_supplier),
+                (TextInputEditText) findViewById(R.id.edit_text_supplier));
 
-            }
+        setListeners("location",
+                (TextInputLayout) findViewById(R.id.input_layout_location),
+                (TextInputEditText) findViewById(R.id.edit_text_location));
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                itemAdapter.getFilter().filter(s);
-            }
+        setListeners("destination",
+                (TextInputLayout) findViewById(R.id.input_layout_destination),
+                (TextInputEditText) findViewById(R.id.edit_text_destination));
 
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        setListeners("quality",
+                (TextInputLayout) findViewById(R.id.input_layout_quality),
+                (TextInputEditText) findViewById(R.id.edit_text_quality));
     }
 
     @Override
@@ -106,12 +88,48 @@ public class NewStockActivity extends AppCompatActivity {
         }
     }
 
-    private void inputOnClick(String rowName) {
+    private void setListeners(final String name, final TextInputLayout inputLayout, final TextInputEditText editText) {
+        inputLayout.setEndIconVisible(false);
 
-        for (String view : inputViews.keySet()) {
-            if (!view.equals(rowName)) {
-                //noinspection ConstantConditions
-                findViewById(inputViews.get(view)).setVisibility(View.GONE);
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    inputLayout.setEndIconVisible(true);
+                    openSearch(name);
+                }
+            }
+        });
+
+        inputLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+                editText.clearFocus();
+                inputLayout.setEndIconVisible(false);
+                cancelSearch();
+            }
+        });
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                itemAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+    }
+
+    private void openSearch(String rowName) {
+
+        for (Integer view : inputViews.values()) {
+            if (!view.equals(inputViews.get(rowName))) {
+                findViewById(view).setVisibility(View.GONE);
             }
         }
 
@@ -124,14 +142,14 @@ public class NewStockActivity extends AppCompatActivity {
         }
         itemAdapter.notifyDataSetChanged();
 
-        recyclerViewWrapper.setVisibility(View.VISIBLE);
+        searchResultsLayout.setVisibility(View.VISIBLE);
     }
 
-    private void cancelInputProductOnClick() {
+    private void cancelSearch() {
         for (int view : inputViews.values()) {
             findViewById(view).setVisibility(View.VISIBLE);
         }
 
-        recyclerViewWrapper.setVisibility(View.GONE);
+        searchResultsLayout.setVisibility(View.GONE);
     }
 }
