9c0102b5484402765479c3dd538df41104a9df13
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-06-23 19:44:53 +0100

    exchange: choose currencies activity

---
+++ app/src/main/java/com/martinwalls/nea/ChooseCurrenciesActivity.java
@@ -0,0 +1,15 @@
+package com.martinwalls.nea;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseCurrenciesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_currencies);
+        //todo recycler views with item_choose_currencies
+        //todo star/unstar change drawable
+    }
+}
