388c106eeaa48ee5f9190954ec4e56abe3151c0f
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-09-16 18:53:48 +0100

    refactor mergesort to reduce duplicate code

--- app/src/main/java/com/martinwalls/nea/Utils.java
+++ app/src/main/java/com/martinwalls/nea/Utils.java
@@ -49,41 +49,7 @@ public class Utils {
      * that implement {@link Comparable}.
      */
     public static <T extends Comparable<T>> List<T> mergeSort(@NonNull List<T> list) {
-        if (list.size() <= 1) {
-            return list;
-        }
-
-        int midpoint = list.size() / 2;
-        List<T> firstHalf = list.subList(0, midpoint);
-        List<T> lastHalf = list.subList(midpoint, list.size());
-
-        firstHalf = mergeSort(firstHalf);
-        lastHalf = mergeSort(lastHalf);
-
-        int i = 0, j = 0;
-        List<T> merged = new ArrayList<>();
-
-        while (i < firstHalf.size() && j < lastHalf.size()) {
-            if (firstHalf.get(i).compareTo(lastHalf.get(j)) < 0) {
-                merged.add(firstHalf.get(i));
-                i++;
-            } else {
-                merged.add(lastHalf.get(j));
-                j++;
-            }
-        }
-
-        while (i < firstHalf.size()) {
-            merged.add(firstHalf.get(i));
-            i++;
-        }
-
-        while (j < lastHalf.size()) {
-            merged.add(lastHalf.get(j));
-            j++;
-        }
-
-        return merged;
+        return mergeSort(list, (o1, o2) -> o1.compareTo(o2));
     }
 
     /**
