af409198dd51406a5060114d0d04877547bdfbbc
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-12-03 19:09:08 +0000

    started ViewModel for stock

---
+++ app/src/main/java/com/martinwalls/meatmanager/data/viewmodel/StockViewModel.java
@@ -0,0 +1,72 @@
+package com.martinwalls.meatmanager.data.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.martinwalls.meatmanager.data.db.DBHandler;
+import com.martinwalls.meatmanager.data.models.StockItem;
+import com.martinwalls.meatmanager.util.SortUtils;
+
+import java.util.List;
+
+public class StockViewModel extends AndroidViewModel {
+
+    private final DBHandler dbHandler;
+
+    private final MutableLiveData<List<StockItem>> dbStockList;
+
+    private final MediatorLiveData<List<StockItem>> stockListObservable;
+
+    private int sortMode = SortUtils.SORT_NAME;
+
+    public StockViewModel(Application application) {
+        super(application);
+
+        dbHandler = new DBHandler(application);
+
+        dbStockList = new MutableLiveData<>();
+
+        stockListObservable = new MediatorLiveData<>();
+
+        stockListObservable.addSource(dbStockList, stockItems ->
+                stockListObservable.setValue(sortStockList(stockItems, sortMode)));
+    }
+
+    public void sortStock(int sortMode) {
+        this.sortMode = sortMode;
+
+        List<StockItem> stockItems = stockListObservable.getValue();
+        if (stockItems == null) {
+            return;
+        }
+
+        stockListObservable.setValue(sortStockList(stockItems, sortMode));
+    }
+
+    private List<StockItem> sortStockList(List<StockItem> stockItems, int sortMode) {
+        switch (sortMode) {
+            case SortUtils.SORT_NAME:
+                return SortUtils.mergeSort(stockItems, StockItem.comparatorAlpha());
+            case SortUtils.SORT_LOCATION:
+                return SortUtils.mergeSort(stockItems, StockItem.comparatorLocation());
+            case SortUtils.SORT_AMOUNT_DESC:
+                return SortUtils.mergeSort(stockItems, StockItem.comparatorAmount(false));
+            case SortUtils.SORT_AMOUNT_ASC:
+                return SortUtils.mergeSort(stockItems, StockItem.comparatorAmount(true));
+        }
+        return stockItems;
+    }
+
+    public void loadStock() {
+        dbStockList.setValue(dbHandler.getAllStock());
+    }
+
+    public LiveData<List<StockItem>> getStockListObservable() {
+        loadStock();
+        return stockListObservable;
+    }
+}
